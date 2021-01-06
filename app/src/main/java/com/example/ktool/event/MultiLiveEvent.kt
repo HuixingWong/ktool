41e22cd154e52b1ffe125a7d42ee922335cd574d
HuixingWang <whx24@live.com>
2021-01-06 14:43:51 +0800

    MultiLiveEvent

---
+++ app/src/main/java/com/example/ktool/event/MultiLiveEvent.kt
@@ -0,0 +1,44 @@
+package com.example.ktool.event
+
+import androidx.lifecycle.*
+
+class MultiLiveEvent<T> : MutableLiveData<T>() {
+
+    private val liveDataToObserve: LiveData<T>
+    private val pendingMap: MutableMap<Int, Boolean>
+
+    init {
+        val outputLiveData = MediatorLiveData<T>()
+        outputLiveData.addSource(this) { currentValue ->
+            outputLiveData.value = currentValue
+        }
+        liveDataToObserve = outputLiveData
+        pendingMap = HashMap()
+    }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+
+        pendingMap[observer.hashCode()] = false
+
+        // Observe the internal MutableLiveData
+        liveDataToObserve.observe(
+            owner,
+            Observer { t ->
+                if (pendingMap[observer.hashCode()] == true) { // don't trigger if the observer wasn't registered
+                    observer.onChanged(t)
+                    pendingMap[observer.hashCode()] = false
+                }
+            }
+        )
+    }
+
+    override fun postValue(value: T) {
+        pendingMap.forEach { pendingMap[it.key] = true }
+        super.postValue(value)
+    }
+
+    override fun setValue(t: T?) {
+        pendingMap.forEach { pendingMap[it.key] = true }
+        super.setValue(t)
+    }
+}
