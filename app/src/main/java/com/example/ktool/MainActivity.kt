7150e38f8c1d496711d7e7c150a3fbfe9ede206c
HuixingWang <whx24@live.com>
2021-01-06 14:39:38 +0800

    commit

--- app/src/main/java/com/example/ktool/MainActivity.kt
+++ app/src/main/java/com/example/ktool/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ktool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ktool.databinding.ActivityMainBinding
+import com.example.ktool.delegate.contentView
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by contentView(R.layout.activity_main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.apply {
+            bird = Bird("🐧")
+        }
     }
 }
+data class Bird (var name: String)
