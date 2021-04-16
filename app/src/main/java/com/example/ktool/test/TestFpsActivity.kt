c003de81e99b9b08ec1efb1c5d35cc1448c3d49d
huixing <wanghuixing24@gmail.com>
2021-04-16 20:51:37 +0800

    test fps

--- app/src/main/java/com/example/ktool/test/TestFpsActivity.kt
+++ app/src/main/java/com/example/ktool/test/TestFpsActivity.kt
@@ -1,12 +1,14 @@
 package com.example.ktool.test
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.ViewParent
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.ktool.R
 import com.example.ktool.ext.log
+import com.example.ktool.source.FpsMonitor
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -21,6 +23,27 @@ class TestFpsActivity : AppCompatActivity() {
             delay(3000)
             //这里的打印结果是 ViewRootImpl
             test()
+        }
+
+        testFps()
+    }
+
+    var current = 0
+    var colorList = listOf(Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED, Color.MAGENTA, Color.BLACK)
+    private fun testFps() {
+        findViewById<Button>(R.id.start).setOnClickListener {
+            FpsMonitor.startMonitor { fps ->
+                findViewById<Button>(R.id.show).apply {
+                    text = String.format("fps: %s", fps)
+                    setTextColor(colorList[(current++) % 6])
+                }
+            }
+
+            Thread.sleep(2000)
+        }
+        findViewById<Button>(R.id.stop).setOnClickListener {
+            FpsMonitor.stopMonitor()
+            findViewById<Button>(R.id.show).text = ""
         }
     }
 
