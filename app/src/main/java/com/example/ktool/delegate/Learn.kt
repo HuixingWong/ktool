cc1eb5595788f062cad22dc41b7a99f1e2b18ac4
huixing <wanghuixing24@gmail.com>
2021-04-19 17:15:15 +0800

    test delegate

--- app/src/main/java/com/example/ktool/delegate/Learn.kt
+++ app/src/main/java/com/example/ktool/delegate/Learn.kt
@@ -1,5 +1,6 @@
 package com.example.ktool.delegate
 
+import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class Example {
@@ -7,11 +8,45 @@ class Example {
 }
 
 class Delegate {
+    var value: String?= ""
     operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return "$thisRef, thank you for delegating '${property.name}' to me!"
+        return "$value, thank you for delegating '${property.name}' to me!"
     }
 
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        this.value = value
         println("$value has been assigned to '${property.name}' in $thisRef.")
     }
+}
+
+class FormatDelegate : ReadWriteProperty<Any?, String> {
+    private var formattedString: String = ""
+
+    override fun getValue(
+        thisRef: Any?,
+        property: KProperty<*>
+    ): String {
+        return formattedString
+    }
+
+    override fun setValue(
+        thisRef: Any?,
+        property: KProperty<*>,
+        value: String
+    ) {
+        formattedString = value.toLowerCase().capitalize()
+    }
+}
+
+class Person(name: String, lastname: String) {
+    var name: String by FormatDelegate()
+    var lastname: String by FormatDelegate()
+    var updateCount = 0
+}
+
+fun main() {
+    var p: String by Delegate()
+    p = "123"
+    println(p)
+//    println(p)
 }
